cf030918246c115000cd2ea5e3b1ec6608317af0
Clarmy Lee <clarmylee92510@gmail.com>
2024-01-30 18:37:07 +0800

    chore: bump the version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(requirements_path) as f:
 
 setuptools.setup(
     name="pymetaf",
-    version="1.0.0",
+    version="1.0.1",
     author="Wentao Li",
     author_email="wentao.li@moji.com",
     description="A python package for parsing metar & taf raw text",
